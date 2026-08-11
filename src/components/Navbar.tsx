1d908553d2a68ae26a484de894c3f0aad88e49aa
mhdalbukhori88 <mhdalbukhori026@gmail.com>
2026-08-11 14:36:22 +0700

    Update logo to newest PNG version

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -42,7 +42,7 @@ export default function Navbar() {
         {/* Logo */}
         <a href="#beranda" className="relative z-50 flex items-center gap-2">
           <Image
-            src="/logo.svg"
+            src="/logo.png"
             alt="XL Satu Binjai"
             width={140}
             height={46}
